f8866026a6732b11fd120c9cf2d132eba52622df
Alejandro Ibañez <JoseIbaez>
2020-11-12 00:47:08 -0600

    Cambiado color

--- views/home.php
+++ views/home.php
@@ -17,7 +17,7 @@
         <?php endif ?>
 
         <?php if ($_SESSION["rol"] == "Usuario") : ?>
-            <div class="alert alert-danger" role="alert">
+            <div class="alert alert-warning" role="alert">
                 <h4 class="alert-heading">Bienvenido de nuevo Usuario: <?= $_SESSION["nombre"] ?></h4>
                 <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
                 <hr>
