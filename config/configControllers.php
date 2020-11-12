17244605302583fe4ec90a286d9fefe63d575835
Alejandro Ibañez <JoseIbaez>
2020-11-12 00:42:10 -0600

    Archivos Agregados

--- config/configControllers.php
+++ config/configControllers.php
@@ -1,5 +1,5 @@
 <?php     
-    define ('BASE_DIR', "http://localhost/finalTPI/");
+    define ('BASE_DIR', "http://localhost/Final_TPI/");
     define ('CONTROLLERS_DIR', "controllers/");
     define ('DEFAULT_CONTROLLER', "Home");
     define ('DEFAULT_ACTION', "login");
