ff1ff8f872b435ee3e046acf363b5efd7df6c1e3
Alejandro Ibañez <JoseIbaez>
2020-11-14 22:44:53 -0600

    Modelo peliculas creado

--- models/Pelicula.php
+++ models/Pelicula.php
@@ -1,7 +1,141 @@
 <?php
 
-
 class Pelicula
 {
-    private $idPelicula;    
+    private $idPelicula;   
+    private $nombre;
+    private $descripcion;
+    private $idCategoria;
+    private $idioma;
+    private $subtitulos;
+    private $director;
+    private $elenco;
+    private $fechaEstreno;
+    private $IdCalidad;
+    private $precioCompra;
+    private $precioAlquiler;
+    private $stock;
+    private $imagen;
+    private $disponibilidad;
+
+    public function set_Id_Pelicula($idPelicula) {
+        $this->idPelicula = $idPelicula;
+    }
+
+    public function get_Id_Pelicula() {
+        return $this->idPelicula;
+    }    
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }    
+
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }    
+
+    public function set_Id_Categoria($idCategoria) {
+        $this->idCategoria = $idCategoria;
+    }
+
+    public function get_Id_Categoria() {
+        return $this->idCategoria;
+    }
+
+    public function setIdioma($idioma) {
+        $this->idioma = $idioma;
+    }
+
+    public function getIdioma() {
+        return $this->idioma;
+    } 
+
+    public function setSubtitulos($subtitulos) {
+        $this->subtitulos = $subtitulos;
+    }
+
+    public function getSubtitulos() {
+        return $this->subtitulos;
+    } 
+
+    public function setDirector($director) {
+        $this->director = $director;
+    }
+
+    public function getDirector() {
+        return $this->director;
+    } 
+
+    public function setElenco($elenco) {
+        $this->elenco = $elenco;
+    }
+
+    public function getElenco() {
+        return $this->elenco;
+    } 
+
+    public function setfechaEstreno($fechaEstreno) {
+        $this->fechaEstreno = $fechaEstreno;
+    }
+
+    public function getfechaEstreno() {
+        return $this->fechaEstreno;
+    } 
+
+    public function set_Id_Calidad($IdCalidad) {
+        $this->$IdCalidad = $IdCalidad;
+    }
+
+    public function get_Id_Calidad() {
+        return $this->IdCalidad;
+    }    
+
+    public function setprecioCompra($precioCompra) {
+        $this->precioCompra = $precioCompra;
+    }
+
+    public function getprecioCompra() {
+        return $this->precioCompra;
+    } 
+
+    public function setprecioAlquiler($precioAlquiler) {
+        $this->precioAlquiler = $precioAlquiler;
+    }
+
+    public function getprecioAlquiler() {
+        return $this->precioAlquiler;
+    } 
+
+    public function setStock($Stock) {
+        $this->stock = $Stock;
+    }
+
+    public function getStock() {
+        return $this->stock;
+    } 
+
+    public function setImagen($imagen) {
+        $this->imagen = $imagen;
+    }
+
+    public function getImagen() {
+        return $this->imagen;
+    } 
+
+    public function setDisponibilidad($disponibilidad) {
+        $this->disponibilidad = $disponibilidad;
+    }
+
+    public function getDisponibilidad() {
+        return $this->disponibilidad;
+    } 
+
 }
---
+++ models/Usuario.php
@@ -0,0 +1,172 @@
+<?php
+require_once 'database/Conexion.php';
+
+class Usuario extends Conexion
+{
+    private $nombre;
+    private $pass;
+    private $apellido;
+    private $Direccion;
+    private $email;
+    private $age;
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setPassword($pass, $clave)
+    {
+        switch ($clave) {
+            case 1: {
+                    $this->pass = password_hash($pass, PASSWORD_DEFAULT);
+                    break;
+                }
+
+            case 2: {
+                    $this->pass = $pass;
+                    break;
+                }
+        }
+    }
+
+    public function getPassword()
+    {
+        return $this->pass;
+    }
+
+    public function setApellido($apelido)
+    {
+        $this->apellido = $apelido;
+    }
+
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    public function setDireccion($Direccion)
+    {
+        $this->Direccion = $Direccion;
+    }
+
+    public function getDireccion()
+    {
+        return $this->Direccion;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function home(){
+        $sql_leer = 'SELECT * FROM pelis';
+        $list = $this->conn->prepare($sql_leer);
+        $list->execute();
+
+        $resultado = $list->fetchAll();
+
+        return $resultado;
+    }
+
+    public function login()
+    {
+        $sql = "SELECT usuarios.email, usuarios.nombre, roles.cargo, usuarios.idUsuario, usuarios.pass FROM usuarios INNER JOIN roles ON usuarios.idRol= roles.idRol  WHERE email = :email";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":email", $this->getEmail());
+
+        if ($stmt->execute()) {
+            $nRow = $stmt->rowCount();
+            if ($nRow == 1) {
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if (password_verify($this->getPassword(), $result["pass"])) {
+                    session_start();
+                    $_SESSION["nombre"] = $result["nombre"];
+                    $_SESSION["id"] = $result["idUsuario"];                                        
+                    $_SESSION["rol"] = $result["cargo"];
+
+                    header("Location: " . BASE_DIR . "Home/home");
+                }else{
+                    return;
+                }
+            }
+        }
+        echo "
+        <script>
+            document.getElementById('error').innerHTML='Error! usuario o contraseña incorrectos';
+            setTimeout(function() {
+                document.getElementById('error').innerHTML='';
+            }, 3000);
+        </script>";
+    }
+
+    public function logout()
+    {
+        //DESTRUIR TODAS LAS VARIABLES DE SESSION
+        $_SESSION = array();
+
+        //SI SE DESEA DESTRUIR LA SESION COMPLETAMENTE, BORRE TAMBIEN LAS COOKIE DE LA SESION
+        //NOTA: ESTO DESTRUIRA LA SESION PERO NO LA INFORMACION DE LA SESION
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
+
+        //FINALMENTE DESTRUIMOS LA SESION
+        session_destroy();
+
+        header('location:' . BASE_DIR);
+    }
+
+    public function signup()
+    {
+        $result = false;
+
+        $query = "INSERT INTO usuarios (nombre, apellido, email, direccion, pass)";
+        $query .= "VALUES('".$this->getNombre()."', '".$this->getApellido()."', '".$this->getEmail()."', '".$this->getDireccion()."','".$this->getPassword()."')";
+
+        $sql = "SELECT * FROM usuarios WHERE email = :email";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":email", $this->getEmail());
+
+        if ($stmt->execute()) {
+            $nRow = $stmt->rowCount();
+            if ($nRow == 1) {
+            }else{
+                if($stmt = $this->conn->query($query)){
+                    $result = true;
+                }
+            }
+        }
+        return $result;
+    }
+}
