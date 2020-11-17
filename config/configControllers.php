2bc256cb6a2db3ec344cd5c3bf4c6c9b9cd28ab8
adolfo <saguzman315@gmail.com>
2020-11-16 20:35:11 -0600

    Creacion de assets

--- config/configControllers.php
+++ config/configControllers.php
@@ -1,5 +1,5 @@
 <?php     
-    define ('BASE_DIR', "http://localhost/Final_TPI/");
+    define ('BASE_DIR', "http://localhost:8080/Final_TPI/");
     define ('CONTROLLERS_DIR', "controllers/");
     define ('DEFAULT_CONTROLLER', "Home");
     define ('DEFAULT_ACTION', "home");
--- views/header.php
+++ views/header.php
@@ -6,6 +6,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="views/assets/css/estilos.css">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -15,7 +17,8 @@
   <body>
     <?php  session_start() ?>
     <div class="p-5">
-      <h1 class="w-50 d-inline-block"><i class="fas fa-dove fa-lg fa-fw"></i>CinePalomadasHD</h1>
+      <h1 class="w-50 d-inline-block titulo_css"><i class="fas fa-dove fa-lg fa-fw titulo_css"></i>CinePalomadasHD</h1>
+      <h2 class="titulo_css">Nuevo</h2>
       <?php if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) { ?>
         <a class="float-right" href="<?= BASE_DIR . "Home/Salir" ?>">Salir</a>
         <?php if ($_SESSION["rol"] != "Usuario") : ?>
