09efa1dec98dd5be08f813705eae9a40ba10dd1a
Marlon Coreas <marlon.coreasv@gmaul.com>
2020-11-23 10:42:52 -0600

    se validaron las sesiones antes de ir a los formularios de login y signup

--- views/login.php
+++ views/login.php
@@ -1,26 +1,30 @@
-<main>
-    <div class="row">
-        <div class="mx-auto col-md-6">
-            <h1>Login Usuario</h1>
-            <form action="<?= BASE_DIR ?>Home/login" method="POST">
-                <div class="form-group">
-                    <label for="">Email</label>
-                    <input type="email" class="form-control" name="email" placeholder="Ingrese email" required>
-                </div>
+<?php if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) {
+    header("Location: " . BASE_DIR . "Home/home"); 
+}else{ ?>
+    <main>
+        <div class="row">
+            <div class="mx-auto col-md-6">
+                <h1>Login Usuario</h1>
+                <form action="<?= BASE_DIR ?>Home/login" method="POST">
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="email" class="form-control" name="email" placeholder="Ingrese email" required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="">Password</label>
-                    <input type="password" class="form-control" name="contra" placeholder="Ingrese contraseña" required>
-                </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="password" class="form-control" name="contra" placeholder="Ingrese contraseña" required>
+                    </div>
 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </div>
-                <!-- <p id="error"></p> -->
-                <div id="error" class="alert alert-danger d-none" role="alert">
-                    Error! usuario o contraseña incorrectos
-                </div>
-            </form>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                    <!-- <p id="error"></p> -->
+                    <div id="error" class="alert alert-danger d-none" role="alert">
+                        Error! usuario o contraseña incorrectos
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
+<?php } ?>
--- views/signup.php
+++ views/signup.php
@@ -1,38 +1,42 @@
-<main>
-    <div class="row">
-        <div class="mx-auto col-md-6">
-            <h1>Guardar Usuario</h1>
-            <form method="POST">
-                <div class="form-group">
-                    <label for="">Nombre</label>
-                    <input type="text" name="nombre" class="form-control" placeholder="Ingrese nombre" required>
-                </div>
+<?php if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) {
+    header("Location: " . BASE_DIR . "Home/home"); 
+}else{ ?>
+    <main>
+        <div class="row">
+            <div class="mx-auto col-md-6">
+                <h1>Guardar Usuario</h1>
+                <form method="POST">
+                    <div class="form-group">
+                        <label for="">Nombre</label>
+                        <input type="text" name="nombre" class="form-control" placeholder="Ingrese nombre" required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="">Apellido</label>
-                    <input type="text" name="apellido" class="form-control" placeholder="Ingrese apellido" required>
-                </div>
+                    <div class="form-group">
+                        <label for="">Apellido</label>
+                        <input type="text" name="apellido" class="form-control" placeholder="Ingrese apellido" required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="">Direccion</label>
-                    <input type="text" name="direccion" class="form-control" placeholder="Ingrese direccion" required>
-                </div>
+                    <div class="form-group">
+                        <label for="">Direccion</label>
+                        <input type="text" name="direccion" class="form-control" placeholder="Ingrese direccion" required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="">Email</label>
-                    <input type="email" class="form-control" name="email" placeholder="Ingrese email" required>
-                </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="email" class="form-control" name="email" placeholder="Ingrese email" required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="">Contraseña</label>
-                    <input type="password" class="form-control" name="contra" placeholder="Ingrese contraseña" required>
-                </div>
+                    <div class="form-group">
+                        <label for="">Contraseña</label>
+                        <input type="password" class="form-control" name="contra" placeholder="Ingrese contraseña" required>
+                    </div>
 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Guardar</button>
-                </div>
-                <p id="error"></p>
-            </form>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </div>
+                    <p id="error"></p>
+                </form>
+            </div>
         </div>
-    </div>
-</main>
+    </main>
+<?php } ?>
