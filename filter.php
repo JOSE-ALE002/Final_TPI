f50364e18b4c418f97fd6630c4120811d30117a2
Marlon Coreas <marlon.coreasv@gmaul.com>
2020-11-23 11:53:19 -0600

    correccion de errores

--- filter.php
+++ filter.php
@@ -1,8 +1,10 @@
 <?php
+  require_once "config/configControllers.php";
   $html = "";
   require_once "models/Pelicula.php";
   $pelicula = new Pelicula();
   $moviesList = array();
+  session_start();
 
   if (isset($_REQUEST['movies']) || isset($_REQUEST['orderBy']) || isset($_REQUEST['popularity'])) {
   
@@ -24,13 +26,31 @@
       $html .= '<div class="row">';
       foreach ($moviesList as $key) :
       $html .= '<div class="card mt-4 ml-2">
-                  <a href="' ."Pelicula/movie&id=" . $key["idPelicula"].'"><img src="'.$key["imagen"].'" style="width: 18rem; height: 26rem;"/></a>
+                  <a href="'.BASE_DIR ."Pelicula/movie&id=" . $key["idPelicula"].'"><img src="'.$key["imagen"].'" style="width: 18rem; height: 26rem;"/></a>
                   <div class="card-body text-warning">
-                  <a href="'. "Home/login" . '" class="text-danger">
+                  <div class="row">';
+                  if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) {
+                    if ($pelicula->verifyLike($key["idPelicula"], $_SESSION["id"])) {
+                      $html .= '<a href="'.BASE_DIR . "Home/dislike&id=" . $key["idPelicula"] . "&idUser=" . $_SESSION["id"] .'" type="button" class="text-danger"
+                      style="color: white;">
+                          <i class="fas fa-heart"></i>
+                      </a>';
+                    } else {
+                      $html .= '<a href="' . BASE_DIR . "Home/like&id=" . $key["idPelicula"] . "&idUser=" . $_SESSION["id"] . '" type="button" class="text-danger"
+                        style="color: white;">
+                            <i class="far fa-heart"></i>
+                        </a>';
+                    }
+                  }else{
+                    $html .= '<a href="'.BASE_DIR. "Home/login" . '" class="text-danger">
                       <i class="far fa-heart"></i>
-                  </a>
+                  </a>';
+                  }
+                  $html .= '
                   <!--Es ejemplo de la cantidad de likes-->
-                  10,698
+                <div>10,698</div>
+                <div class="col"><span class="float-right"> $ '.$key["precioCompra"].'</span></div>
+                </div>
               </div>
         </div>';
       endforeach;
@@ -38,4 +58,9 @@
     }
   }
   echo $html;
+
+
+
+
+  
 ?>
