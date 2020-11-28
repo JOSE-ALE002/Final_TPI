6f32eeb77dd8304c417da12027373540d7ec3469
Alejandro Ibañez <JoseIbaez>
2020-11-28 16:40:23 -0600

    Login estilizado

--- views/header.php
+++ views/header.php
@@ -11,6 +11,7 @@
 
     <!-- Eslilos propios -->
     <link rel="stylesheet" href="<?= BASE_DIR ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= BASE_DIR ?>assets/css/styleLogin.css">
 
     <!-- Font Awesome 5 -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
