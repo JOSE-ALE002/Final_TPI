4a9a6b5fedefd3b8c9e666298bf9e469d7c52166
Alejandro Ibañez <JoseIbaez>
2020-11-28 20:43:01 -0600

    Navbar arreglado

--- views/navbar.php
+++ views/navbar.php
@@ -9,33 +9,32 @@
           <form class="form-inline">
             <label for="" class="text-white pr-3 w-25">Buscar: </label>
             <input class="form-control w-75" name="search" id="search" placeholder="Search" aria-label="Search">
+          </form>
         </div>
-        </form>
-      </div>
-      <div class="col-md-3">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <div class="navbar-nav ml-auto">
-            <?php if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) { ?>
-              <?php if ($_SESSION["rol"] != "Usuario") : ?>
-                <a class="nav-link" href="<?= BASE_DIR . "Pelicula/save" ?>">Agregar Pelicula</a>
-              <?php endif ?>
+        <div class="col-md-3">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav ml-auto">
+              <?php if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) { ?>
+                <?php if ($_SESSION["rol"] != "Usuario") : ?>
+                  <a class="nav-link" href="<?= BASE_DIR . "Pelicula/save" ?>">Agregar Pelicula</a>
+                <?php endif ?>
 
-              <?php if ($_SESSION["rol"] == "Usuario") : ?>
-                <a class="nav-link" href="<?= BASE_DIR . "Home/favoritos&idUser=" . $_SESSION["id"] ?>">
-                  Peliculas Favoritas
-                </a>
-                <a class="nav-link active" href="<?= BASE_DIR . "Home/Salir" ?>">
-                  Salir
-                </a>
-              <?php endif ?>
-            <?php } else { ?>
-              <a class="nav-link" href="<?= BASE_DIR . "Home/login" ?>">Log In</a>
-              <a class="nav-link" href="<?= BASE_DIR . "Home/signup" ?>">Sign Up</a>
-            <?php } ?>
+                <?php if ($_SESSION["rol"] == "Usuario") : ?>
+                  <a class="nav-link" href="<?= BASE_DIR . "Home/favoritos&idUser=" . $_SESSION["id"] ?>">
+                    Peliculas Favoritas
+                  </a>
+                  <a class="nav-link active" href="<?= BASE_DIR . "Home/Salir" ?>">
+                    Salir
+                  </a>
+                <?php endif ?>
+              <?php } else { ?>
+                <a class="nav-link" href="<?= BASE_DIR . "Home/login" ?>">Log In</a>
+                <a class="nav-link" href="<?= BASE_DIR . "Home/signup" ?>">Sign Up</a>
+              <?php } ?>
+            </div>
           </div>
         </div>
       </div>
+    </nav>
   </div>
-  </nav>
-</div>
 </div>
