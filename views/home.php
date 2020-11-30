d01af7ebae37ddccd2b64c9d8ce2336799aef664
Alejandro Ibañez <JoseIbaez>
2020-11-30 17:59:54 -0600

    arreglado detalles finales

--- views/home.php
+++ views/home.php
@@ -92,13 +92,13 @@
                                         $p = new Pelicula();
 
                                         if ($p->verifyLike($key["idPelicula"], $_SESSION["id"])) { ?>
-                                            <a href="#" class="like-count text-danger" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="like">
+                                            <a href="#" class="like-count text-danger text-decoration-none" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="like">
                                                 <i class="fas fa-heart"></i>
                                                 <span class="text-warning"><?php echo $p->countLikes($key["idPelicula"]); ?></span>
                                             </a>
                                         <?php
                                         } else { ?>
-                                            <a href="#" class="like-count text-danger" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="dislike">
+                                            <a href="#" class="like-count text-danger text-decoration-none" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="dislike">
                                                 <i class="far fa-heart"></i>
                                                 <span class="text-warning"><?php echo $p->countLikes($key["idPelicula"]); ?></span>
                                             </a>
@@ -188,7 +188,7 @@
 
                                     <div class="row">
                                         <div class="col-md-6 text-warning">
-                                            <a href="#" class="like-count sidebar text-danger" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="like">
+                                            <a href="#" class="like-count sidebar text-dange text-decoration-none" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="like">
                                                 <i class="far fa-heart"></i>
                                                 <span class="text-warning"><?php echo $pelicula->countLikes($key["idPelicula"]); ?></span>
                                             </a>
@@ -208,7 +208,7 @@
 
                                     <div class="row">
                                         <div class="col-md-6 text-warning">
-                                            <a href="#" class="like-count sidebar text-danger" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="like">
+                                            <a href="#" class="like-count sidebar text-danger text-decoration-none" data-idpelicula=<?php echo $key['idPelicula']; ?> data-idusuario="<?php echo $_SESSION['id']; ?>" data-estado="like">
                                                 <i class="far fa-heart"></i>
                                                 <span class="text-warning"><?php echo $pelicula->countLikes($key["idPelicula"]); ?></span>
                                             </a>
