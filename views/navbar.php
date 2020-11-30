3b12e209f03fc93dc35c5015ef4ed24823c950a1
Alejandro Ibañez <JoseIbaez>
2020-11-30 16:01:59 -0600

    Arreglado navbar

--- views/navbar.php
+++ views/navbar.php
@@ -10,51 +10,76 @@
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <div class="row ml-1">
-          <form class="form-inline my-2 my-lg-0">
-            <a class="nav-link text-white" id="buscar" href="#">
-              <i class="fa fa-search" aria-hidden="true"></i>
-            </a>
-            <input class="search_peli" name="search" id="search" type="search" placeholder="Search . . ." aria-label="Search">
-          </form>
-        </div>
-        <ul class="navbar-nav">
-          <!-- <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li> -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Categorias
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
-        <div class="navbar-nav ml-auto">
-          <?php if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) { ?>
-            <?php if ($_SESSION["rol"] != "Usuario") : ?>
+        <?php if (isset($_SESSION["nombre"]) && isset($_SESSION["rol"])) { ?>
+          <?php if ($_SESSION["rol"] != "Usuario") : ?>
+            <div class="navbar-nav ml-auto">
               <a class="nav-link" href="<?= BASE_DIR . "Pelicula/save" ?>">Agregar Pelicula</a>
               <a class="nav-link active" href="<?= BASE_DIR . "Home/Salir" ?>">
                 Salir
               </a>
-            <?php endif ?>
+            </div>
+          <?php endif ?>
 
-            <?php if ($_SESSION["rol"] == "Usuario") : ?>
+          <?php if ($_SESSION["rol"] == "Usuario") : ?>
+            <div class="row ml-1">
+              <form class="form-inline my-2 my-lg-0">
+                <a class="nav-link text-white" id="buscar" href="#">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </a>
+                <input class="search_peli" name="search" id="search" type="search" placeholder="Search . . ." aria-label="Search">
+              </form>
+            </div>
+
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Categorias
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+
+            <div class="navbar-nav ml-auto">
               <a class="nav-link" href="<?= BASE_DIR . "Home/favoritos&idUser=" . $_SESSION["id"] ?>">
                 Peliculas Favoritas
               </a>
               <a class="nav-link active" href="<?= BASE_DIR . "Home/Salir" ?>">
                 Salir
               </a>
-            <?php endif ?>
-          <?php } else { ?>
+            </div>
+          <?php endif ?>
+        <?php } else { ?>
+          <div class="row ml-1">
+            <form class="form-inline my-2 my-lg-0">
+              <a class="nav-link text-white" id="buscar" href="#">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </a>
+              <input class="search_peli" name="search" id="search" type="search" placeholder="Search . . ." aria-label="Search">
+            </form>
+          </div>
+
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Categorias
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+
+          <div class="navbar-nav ml-auto">
             <a class="nav-link" href="<?= BASE_DIR . "Home/login" ?>">Log In</a>
             <a class="nav-link" href="<?= BASE_DIR . "Home/signup" ?>">Sign Up</a>
-          <?php } ?>
-        </div>
+          </div>
+        <?php } ?>
       </div>
     </nav>
   </div>
